58b8ed0430238033e016f9067bceb875eb4d60fd
siyamrohman159-netizen <siyamrohman159@gmail.com>
2026-02-25 21:22:26 +0600

    Refactor index.js to add Express server and clean comments
    Removed warnings and motivational comments in multiple languages from the top of the file. Added a simple Express server to respond to HTTP requests.

--- index.js
+++ index.js
@@ -3,23 +3,25 @@
  * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
  */
 
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
+// 🔥 Fake web server for Render (REQUIRED)
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Siyuuu's bot is running on Render Web Service!");
+});
+
+// Render MUST detect a running web server
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Web service running on port:", process.env.PORT || 3000);
+});
+
+// 🔥 GoatBot auto restart controller
 function startProject() {
 	const child = spawn("node", ["Goat.js"], {
 		cwd: __dirname,
